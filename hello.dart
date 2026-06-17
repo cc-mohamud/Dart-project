cefbe20aa229c52e9c8764f0505f66a04de3e658
ccMohamud <brahimialli991999@gmail.com>
2026-06-17 11:06:24 +0300

    printing hello, basel

--- hello.dart
+++ hello.dart
@@ -1,12 +1,3 @@
-void main() {
-
-    String name = "omar";
-    int age = 21;
-    double height = 6.5;
-
-
-
-    print(name);
-    print(age);
-    print(height);
+void main(){
+    print("Hello, Basel!");
 }
