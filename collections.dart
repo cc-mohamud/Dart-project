11ef9c099c73e8fc9451a4d1b342284542a99ace
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 17:41:01 +0300

    exploring lists in dart

--- collections.dart
+++ collections.dart
@@ -1,4 +1,22 @@
-// List in Dart,
-// Set in Dart,
-// Map in Dart and
-// Where in Dart.
+// void main() {  
+//    var list1 = [210,21,22,33,44,55];  
+//    print(list1);  
+// }  
+
+
+void main() {
+  var list = [210, 21, 22, 33, 44, 55];
+
+  print(list[0]);
+  print(list[1]);
+  print(list[2]);
+  print(list[3]);
+  print(list[4]);
+  print(list[5]);
+}
+
+
+
+
+
+
