927dcf730fa2668a30b81c93d3b64c98d8e7313b
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 12:04:08 +0300

    learning for loop with arithmetics

--- loops.dart
+++ loops.dart
@@ -1,5 +1,12 @@
-void main() {
-  for (int i = 1; i <= 10; i++) {
-    print(i);
+void main(){
+
+  int total = 0;
+  int n = 100; 
+  
+  for(int i=1; i<=n; i++){
+    total = total + i;
   }
+  
+  print("Total is $total");
+  
 }
