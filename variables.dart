2aa12a1a48803de6b365fb6d86f472025e6cb5ac
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 10:03:19 +0300

    Learning dart variables

---
+++ variables.dart
@@ -0,0 +1,14 @@
+void main() {
+// declaring variables
+String name = "John";
+String address = "USA";  
+num age = 20;  
+num height = 5.9;
+bool isMarried = false;
+   
+print("Name is $name");
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married Status is $isMarried");
+}
