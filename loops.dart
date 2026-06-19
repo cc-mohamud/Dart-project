bc518ed5333c507caebfb7d7ec3efe9138b5af28
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 11:57:28 +0300

    printing my name 10x without loop

--- loops.dart
+++ loops.dart
@@ -1,12 +1,5 @@
 void main() {
+  for (int i = 0; i < 10; i++) {
     print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
-    print("John Doe");
+  }
 }
