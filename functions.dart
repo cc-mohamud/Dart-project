7edca0c25760f260373ede3957e743bbd6144318
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 16:25:52 +0300

    fn with return value and prameters

--- functions.dart
+++ functions.dart
@@ -1,9 +1,10 @@
-// Functions are the block of code that performs a specific task. They are created when some statements are repeatedly occurring in the program. The function helps reusability of the code in the program.
-
-void printName() {
-  print("My name is: ccMohamud");
+int? sum(int a, int b) {
+  int total = a + b;
+  return
+    total;
+  
 }
 
 void main() {
-  printName();
+  print("Total is: ${sum(56, 65)}");
 }
