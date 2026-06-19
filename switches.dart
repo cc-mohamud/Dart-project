96d41ea5625727eabcecff63f6208985075eaaaa
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 11:16:21 +0300

    Switch Case On Strings

--- switches.dart
+++ switches.dart
@@ -1,18 +1,39 @@
-void main() {
-  int seatNo = 01;
+// void main() {
+//   int seatNo = 01;
 
-  switch(seatNo){
-    case 01:
-      print("is for basel");
+//   switch(seatNo){
+//     case 01:
+//       print("is for basel");
+//       break;
+//     case 02:
+//       print("is for daud");
+//       break;
+//     case 03:
+//       print("is for halidi");
+//       break;
+//     default:
+//       print("wrong seat");
+//     break;
+//   };
+// }
+
+
+void main() {
+ const weather = "cloudy";
+
+  switch (weather) {
+    case "sunny":
+        print("Its a sunny day. Put sunscreen.");
+        break;
+    case "snowy":
+        print("Get your skis.");
       break;
-    case 02:
-      print("is for daud");
-      break;
-    case 03:
-      print("is for halidi");
+    case "cloudy":
+    case "rainy": 
+      print("Please bring umbrella.");
       break;
     default:
-      print("wrong seat");
-    break;
-  };
+        print("Sorry I am not familiar with such weather.");
+      break;
+  }
 }
