ed2ec9f16a93df3f285cc5538b2469ec5d63dd99
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 11:49:59 +0300

    Observing ternery operators

--- switches.dart
+++ switches.dart
@@ -1,39 +1,8 @@
-// void main() {
-//   int seatNo = 01;
-
-//   switch(seatNo){
-//     case 01:
-//       print("is for basel");
-//       break;
-//     case 02:
-//       print("is for daud");
-//       break;
-//     case 03:
-//       print("is for halidi");
-//       break;
-//     default:
-//       print("wrong seat");
-//     break;
-//   };
-// }
-
-
 void main() {
- const weather = "cloudy";
+  var age = 13;
 
-  switch (weather) {
-    case "sunny":
-        print("Its a sunny day. Put sunscreen.");
-        break;
-    case "snowy":
-        print("Get your skis.");
-      break;
-    case "cloudy":
-    case "rainy": 
-      print("Please bring umbrella.");
-      break;
-    default:
-        print("Sorry I am not familiar with such weather.");
-      break;
-  }
+  var check = (13 <= age && age <= 19) ? "Teenager" : "Not Teenager";
+
+
+  print(check);
 }
