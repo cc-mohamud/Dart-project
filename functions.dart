2d50b6fbabb150105e7c87a54d5797a3bbeb9b51
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 16:16:44 +0300

    printing my name with fn

---
+++ functions.dart
@@ -0,0 +1,9 @@
+// Functions are the block of code that performs a specific task. They are created when some statements are repeatedly occurring in the program. The function helps reusability of the code in the program.
+
+void printName() {
+  print("My name is: ccMohamud");
+}
+
+void main() {
+  printName();
+}
