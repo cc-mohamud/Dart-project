8a46570e6fe7d07e98f471d5dc8d5e59f50cc464
ccMohamud <brahimialli991999@gmail.com>
2026-06-19 17:19:15 +0300

    fn with math in dart

--- functions.dart
+++ functions.dart
@@ -1,12 +1,17 @@
-double calculateInterest(double principle, double rate, double time) => 
-  principle * rate * time / 100;
+import 'dart:math';
+void main()
+{
+  int num1 = 10;
+  int num2 = 2;
 
-
-  void main() {
-    double principle = 50000;
-    double rate = 3;
-    double time = 5;
-
-    double result = calculateInterest(principle, rate, time);
-    print("The simple interest is: $result");
-  }
+  num powernum = pow(num1,num2);
+  num maxnum = max(num1,num2);
+  num minnum = min(num1,num2);
+  num squareroot = sqrt(25); // Square root of 25
+  
+  print("Power is $powernum"); 
+  print("Maximum is $maxnum"); 
+  print("Minimum is $minnum"); 
+  print("Square root is $squareroot"); 
+  
+}
