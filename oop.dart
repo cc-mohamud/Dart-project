90c89d29d5574cf8169b0ea757ca65e960833bb9
ccMohamud <brahimialli991999@gmail.com>
2026-06-20 14:16:04 +0300

    Initial commit of OOP in dart

---
+++ oop.dart
@@ -0,0 +1,12 @@
+class Animal {
+  String? name;
+  int? numberOfLegs;
+  String? lifeSpan;
+
+
+  void display(){
+    print("Animal name is: $name");
+    print("Number of legs: $numberOfLegs");
+    print("Lifespan is: $lifeSpan");
+  }
+}
