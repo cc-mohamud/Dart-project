f1597065ebab59deb7afd54fd0b66dad073784f7
ccMohamud <brahimialli991999@gmail.com>
2026-06-20 18:30:37 +0300

    learned Enum in Dart language

--- oop.dart
+++ oop.dart
@@ -1,18 +1,36 @@
-class Student {
-  int id;
-  String name;
-  static String schoolName = "ABC School";
-  Student(this.id, this.name);
-  void display() {
-    print("Id: ${this.id}");
-    print("Name: ${this.name}");
-    print("School Name: ${Student.schoolName}");
-  }
+enum days {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
 }
 
 void main() {
-  Student s1 = new Student(1, "John");
-  s1.display();
-  Student s2 = new Student(2, "Smith");
-  s2.display();
+  var today = days.Wednesday;
+  switch (today) {
+    case days.Sunday:
+      print("Today is Sunday.");
+      break;
+    case days.Monday:
+      print("Today is Monday.");
+      break;
+    case days.Tuesday:
+      print("Today is Tuesday.");
+      break;
+    case days.Wednesday:
+      print("Today is Wednesday.");
+      break;
+    case days.Thursday:
+      print("Today is Thursday.");
+      break;
+    case days.Friday:
+      print("Today is Friday.");
+      break;
+    case days.Saturday:
+      print("Today is Saturday.");
+      break;
+  }
 }
